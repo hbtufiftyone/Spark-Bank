e81189de098ad92f7b7cb6a63c95dd7cb99aa669
knoldus <chaurasiyasachin887@gmail.com>
2023-05-15 15:32:37 +0530

    f

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def transactions():
 	return render_template('transactions.html',data=rows[::-1])
 
 if __name__=='__main__':
-        app.run(debug=True,port=8080)
+        app.run(host='0.0.0.0')
